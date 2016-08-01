818b872af2b42d35325da5c98a4f582d58984230
Andrews Medina <andrewsmedina@gmail.com>
2016-07-31 21:35:41 -0300

    add more panic and recover examples

---
+++ panic4.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func soma(a, b int) int {
+	if a > 10 {
+		panic("nao e possivel soma de numeros maiores que 10")
+	}
+	return a + b
+}
+
+func main() {
+	fmt.Printf("A soma de 10 + 10 é: %d\n", soma(10, 10))
+	fmt.Printf("A soma de 1 + 4 é: %d\n", soma(1, 4))
+	fmt.Printf("A soma de 11 + 4 é: %d\n", soma(11, 4))
+	fmt.Printf("A soma de 2 + 4 é: %d\n", soma(2, 4))
+}
