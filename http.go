9fd213d2a3d43b36276a1ebfa32370697c6497fd
Andrews Medina <andrewsmedina@gmail.com>
2016-08-02 13:59:44 -0300

    add http recover code

---
+++ http.go
@@ -0,0 +1,17 @@
+// Serve a new connection.
+func (c *conn) serve() {
+	c.remoteAddr = c.rwc.RemoteAddr().String()
+	defer func() {
+		if err := recover(); err != nil {
+			const size = 64 << 10
+			buf := make([]byte, size)
+			buf = buf[:runtime.Stack(buf, false)]
+			c.server.logf("http: panic serving %v: %v\n%s", c.remoteAddr, err, buf)
+		}
+		if !c.hijacked() {
+			c.close()
+			c.setState(c.rwc, StateClosed)
+		}
+	}()
+	// ...
+}
